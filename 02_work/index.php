8341b05abdeaa740c443c45bb91416fc250c4285
Артем Никитин <artem-ahead@yandex.ru>
2023-02-06 23:07:20 +0500

    multiplication

--- 02_work/index.php
+++ 02_work/index.php
@@ -1,14 +1,31 @@
 <?php error_reporting(-1);
 
-$year = 1900;
+//$year = 1900;
+//
+//echo '<select border="1" width="100%">';
+//
+//while ($year <= 2021) {
+//    echo "<option>";
+//    echo $year;
+//    echo "</option>";
+//    $year++;
+//}
+//
+//echo '</select>';
 
-echo '<select border="1" width="100%">';
+echo '<table border="1" width="100%">';
 
-while ($year <= 2021) {
-    echo "<option>";
-    echo $year;
-    echo "</option>";
-    $year++;
+$tr = 1;
+while ($tr <= 9) {
+    echo "<tr>";
+    $td = 1;
+    while ($td <= 9) {
+        $multiplication = $td * $tr;
+        echo "<td> $td * $tr = $multiplication  </td>";
+        $td++;
+    }
+    echo "</tr>";
+    $tr++;
 }
 
-echo '</select>';
+echo '</table>';
